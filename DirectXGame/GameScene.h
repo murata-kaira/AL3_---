2f95fdc7e08b0332aa635ac3ff2b0c31761e7901
murata-kaira <k024g0062@g.neec.ac.jp>
2025-04-14 15:53:53 +0900

    スタート

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "KamataEngine.h"
+
+class GameScene {
+
+	public:
+
+	void Initialize();
+
+
+	void Update();
+
+
+	void Draw();
+
+
+
+
+
+};
+
+
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include"KamataEngine.h"
+#include"GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	KamataEngine::Initialize(L"LE2D_23_ムラタ_カイラ_AL3");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	GameScene* gameScene = new GameScene();
+
+	gameScene->Initialize();
+
+
+	while(true) {
+
+		if (KamataEngine::Update()) {
+		break;
+		}
+
+		gameScene->Update();
+
+		dxCommon->PreDraw();
+
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+
+	}
+
+
+	KamataEngine::Finalize();
+
+
+	delete gameScene;
+
+	gameScene = nullptr;
+
+
+	return 0;
+}
